56e43e38063ebd4e083d8ea18495a4a0fae93f9a
Alimstfa2911 <mdmustafaali@29112000@gmail.com>
2024-12-21 22:15:06 +0530

    Parallel code to canny edge gradient

---
+++ Stack1.java
@@ -0,0 +1,9 @@
+class Stack1{
+	public static void main(String args[]){
+		ArrayDeque <INTEGER> s = new stack<>();
+		s.push(10);
+		s.push(20);
+		s.push(30);
+		System.out.println(s.peek());
+	}
+}
